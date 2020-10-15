9fd4b8bcc43d018e582d965077a978f7afef855c
tmasjiao <tmas.jiao@modooplay.com>
2020-10-15 09:57:47 +0800

    add patch & etc

---
+++ Unity3DExtern/Assets/Scripts/Exercise/CircleRun.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleRun : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    public void Run(int maxcount, int inner) 
+    {
+        var list = new List<int>();
+        for (int i = 0; i < maxcount; i++)
+        {
+            list.Add(i);
+        }
+    }
+}
