5103b213b4bd9e01f2a05b88f83e86a15e0fcb86
tmasjiao <tmas.jiao@modooplay.com>
2020-10-16 10:06:22 +0800

    update

--- Unity3DExtern/Assets/Scripts/Odin/OdinTest.cs
+++ Unity3DExtern/Assets/Scripts/Odin/OdinTest.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using System;
 
-public class OdinTest : MonoBehaviour
+public class OdinTest : SerializedMonoBehaviour
 {
     [AssetsOnly]
     public List<GameObject> m_ListGo;
@@ -11,4 +13,28 @@ public class OdinTest : MonoBehaviour
     public List<GameObject> m_ListGoScene;
     [SerializeReference]
     private Dictionary<int,GameObject> m_DicOdin;
+
+    public Dictionary<int,GameObject> m_DicOdin2;
+
+    public UnitySerialize m_UnityObj;
+    public OdinSerializeTest m_OdinObj;
+    void Start() 
+    {
+        //Debug.Log("m_DicOdin count" + m_DicOdin.Count);
+        Debug.Log("m_DicOdin2 count" + m_DicOdin2.Count);
+    }
+}
+
+[Serializable]
+public class UnitySerialize 
+{
+    [OdinSerialize]
+    public Dictionary<int,GameObject> m_DicOdinUnitySer;
+
+}
+
+public class OdinSerializeTest
+{
+    [OdinSerialize]
+    public Dictionary<int,GameObject> m_DicOdinUnitySer;
 }
